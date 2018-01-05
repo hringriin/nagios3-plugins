310f178695dc0fb5bea75f90768bf29d625835df
root <root@chamus.fritz.box>
2018-01-05 16:26:34 +0000

    [ typos shit ]

--- teamspeak3/link.bash
+++ teamspeak3/link.bash
@@ -3,5 +3,5 @@
 
 source ../config.bash
 
-cp -fv ${REPOURL}/teamspeak3/teamspeak3.cfg ${ETCPLUG}/teamspeak3-plugin.cfg
+cp -fv ${REPOURL}/teamspeak3/teamspeak3-plugin.cfg ${ETCPLUG}/teamspeak3.cfg
 cp -fv ${REPOURL}/teamspeak3/teamspeak3-nagios/check_teamspeak3 ${PLUGINS}/check_teamspeak3
