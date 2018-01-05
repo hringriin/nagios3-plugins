c7363bde21c92a0235e730b03df4d6f9acff023e
Joschka Koester <jkoester@tzi.de>
2018-01-05 17:15:39 +0100

    [ home folder correct ]

--- config.bash
+++ config.bash
@@ -1,6 +1,6 @@
 #!/bin/bash
 # config
 
-REPOURL="/home/${USER}/Repositories/github.com/hringriin/nagios3-plugins"
+REPOURL="~/Repositories/github.com/hringriin/nagios3-plugins"
 ETCPLUG="/etc/nagios-plugins/config"
 PLUGINS="/usr/lib/nagios/plugins"
