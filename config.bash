6e240c395d7931902eb37b845c4345d613987de9
Joschka Koester <jkoester@tzi.de>
2018-01-05 17:18:29 +0100

    grr

--- config.bash
+++ config.bash
@@ -1,6 +1,6 @@
 #!/bin/bash
 # config
 
-REPOURL="~/Repositories/github.com/hringriin/repo/nagios3-plugins"
+REPOURL="~/Repositories/github.com/hringriin/nagios3-plugins/repo"
 ETCPLUG="/etc/nagios-plugins/config"
 PLUGINS="/usr/lib/nagios/plugins"
