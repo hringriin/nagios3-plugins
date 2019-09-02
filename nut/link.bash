04b5f579f78c681d322f00ed23a101de28a5b08e
Joschka Köster <hringriin@gmail.com>
2019-09-02 03:05:56 +0200

    [ check_nut ] typo in linker file

--- nut/link.bash
+++ nut/link.bash
@@ -3,5 +3,5 @@
 
 source ../config.bash
 
-cp -fv ${REPOURL}/nut/chec_nut.cfg ${ETCPLUG}/check_nut.cfg
+cp -fv ${REPOURL}/nut/check_nut.cfg ${ETCPLUG}/check_nut.cfg
 cp -fv ${REPOURL}/nut/check_nut ${PLUGINS}/check_nut
